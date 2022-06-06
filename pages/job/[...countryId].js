40932ce7e731772b106071fb4349d6bf23a023f3
devkd <devkd@debian-BULLSEYE-live-builder-AMD64>
2022-06-06 21:05:32 +0300

    [add] btn add more job

--- pages/job/[...countryId].js
+++ pages/job/[...countryId].js
@@ -7,6 +7,7 @@ import {
   SERVER_URL,
   JOB_URL,
   getFullNameContinentById,
+  BORDER,
 } from "../../components/global";
 import SelectJobCard from "../../components/Job/CardSelectJob";
 import axios from "axios";
@@ -14,6 +15,8 @@ import axios from "axios";
 export default function SelectCard() {
   const [listJob, setListJob] = useState([]);
   const [locationName, setLocationName] = useState();
+  const [indexJobNow, setIndexJobNow] = useState(20);
+  const [isShowBtnLoadMoreJob, setIsShowBtnLoadMoreJob] = useState(true);
 
   // Проверка на страну или континент
   useEffect(async () => {
@@ -28,24 +31,47 @@ export default function SelectCard() {
       case "AU":
       case "AF":
         await axios(`${SERVER_URL}/api/${JOB_URL}/get-jobs/${countryId}`).then(
-          (listJob) => {
-            setListJob(listJob.data.message);
+          (listJobs) => {
+            setListJob(listJobs.data.message);
             // get name location by tag
             setLocationName(getFullNameContinentById(countryId));
+            listJobs.data.message.length === 20
+              ? setIsShowBtnLoadMoreJob(true)
+              : setIsShowBtnLoadMoreJob(false);
           }
         );
         break;
 
       default:
+        // This is county page
         await axios
           .get(`${SERVER_URL}/api/${JOB_URL}/get-jobs/county/${countryId}`)
-          .then((listJob) => {
-            setListJob(listJob.data.message);
-            setLocationName(listJob.data.message[0].location);
+          .then((listJobs) => {
+            setListJob(listJobs.data.message);
+            setLocationName(listJobs.data.message[0].location);
+            listJobs.data.message.length === 20
+              ? setIsShowBtnLoadMoreJob(true)
+              : setIsShowBtnLoadMoreJob(false);
           });
         break;
     }
   }, []);
+
+  const loadMoreJob = async () => {
+    const countryId = window.location.pathname.split("/")[2];
+
+    await axios
+      .post(`${SERVER_URL}/api/${JOB_URL}/get-jobs/${countryId}`, {
+        indexJobNow: indexJobNow,
+      })
+      .then(({ data }) => {
+        const chunkListJob = data.message;
+        // hide btn Load more job
+        setIndexJobNow(indexJobNow + 20);
+        setListJob(listJob.concat(chunkListJob));
+        chunkListJob.length < 20 ? setIsShowBtnLoadMoreJob(false) : null;
+      });
+  };
 
   return (
     <GlobalWrap>
@@ -69,6 +95,11 @@ export default function SelectCard() {
           />
         ))}
       </div>
+      {listJob.length <= 20 && isShowBtnLoadMoreJob ? (
+        <button className="btn" onClick={loadMoreJob}>
+          Загрузить ещё
+        </button>
+      ) : null}
 
       <style jsx>{`
         h1 {
@@ -79,6 +110,20 @@ export default function SelectCard() {
         .wrap {
           display: flex;
           flex-wrap: wrap;
+          margin-bottom: 50px;
+        }
+        .btn {
+          border: ${BORDER};
+          border-radius: 10px;
+          width: 150px;
+          height: 50px;
+          cursor: pointer;
+          margin: 0 auto 50px;
+          color: ${BLUE};
+        }
+        .btn:hover {
+          background-color: ${BLUE};
+          color: #fff;
         }
       `}</style>
     </GlobalWrap>
